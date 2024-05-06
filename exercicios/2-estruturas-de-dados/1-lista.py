e5114cfe40e381ae45fb6063e1d167bd0ba07b01
adrianoglima <adrianoglima@gmail.com>
2024-05-06 19:15:12 +0000

    listas

--- exercicios/2-estruturas-de-dados/1-lista.py
+++ exercicios/2-estruturas-de-dados/1-lista.py
@@ -1,13 +1,17 @@
 # Crie uma lista apenas com elementos numéricos
-
+numeros = [1,2,3,4,5,6]
 # Crie uma lista contendo todos os tipos e estrutura de dados que você aprendeu até agora
-
+misto = [1,"adriano",[2,"lima","dada"], "julia"]
 # Imprima na tela apenas os 5 primeiros elementos da lista
-
+print(numeros[0:5])
 # Crie um slice na lista para que imprima na tela os elementos de índice par
-
+print(numeros[1:-1:2])
 # Remova da lista o último item
-
+misto.pop()
 # Insira na lista um novo item
-
+misto.append('python')
 # Remova da lista um item específico
+numeros.remove(3)
+print('--------------')
+print(numeros)
+print(misto)
--- exercicios/2-estruturas-de-dados/projeto-tempo-para-estudos.py
+++ exercicios/2-estruturas-de-dados/projeto-tempo-para-estudos.py
@@ -1,6 +1,17 @@
 # Criaremos um script que imprimirá na tela o total de horas que uma pessoa estudou durante um determinado período:
 # 1. Crie uma variável chamada "nome" e, usando o método input(), atribua a ela um nome;
+nome = input("Informe um nome: ")
+
 # 2. Crie uma variável chamada "total_dias" e, usando o método input(), solicite o total de dias dedicados ao estudo por semana;
+total_dias = input("Informe o total de dias dedicados por semana: ")
+
 # 3. Crie uma variável chamada "total_horas" e, usanod o método input(), solicite a média de horas estudada por dia;
+total_horas = input("Informe a média de horas estudas por dia: ")
+
 # 4. Crie uma variável chamada "curso" e, usando o método input(), solicite o título do curso desejado;
+curso = input("Informe o nome do curso: ")
+
 # 5. Imprima na tela uma frase informando o nome da estudante, o total_dias dedicados aos estudos, o total horas semanais e o curso.
+total_dias = int(total_dias)
+total_horas = int(total_horas)
+print(f"O Estudante {nome} dedica {total_dias} por semana, totalizando {total_horas*total_dias} no curso de: '{curso}'")
