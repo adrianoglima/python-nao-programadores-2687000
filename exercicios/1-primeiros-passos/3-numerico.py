0ae24719091de34d628fe3b06434cdad957e6490
adrianoglima <adrianoglima@gmail.com>
2024-05-06 14:50:32 +0000

    manipulando variaveis

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -3,9 +3,16 @@ idade_numerica = 46
 altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
-
+print(type(data_nascimento))
+print(type(idade_numerica))
+print(type(altura))
 
 # Realize uma operação entre dados do tipo string e inteiro
-
+print(data_nascimento * 3)
+print(altura + idade_numerica)
 
 # Realize uma operação entre dados do tipo inteiro e float
+inteiro = 23
+minha_altura = 1.79
+arredondar = round(minha_altura,0)
+print(f'Minha altura arredondada é {arredondar} e eu tenho {23+20} anos')
